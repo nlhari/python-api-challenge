a78062303145d11c8c8fe360edf44b943f8a1925
nlhari <55560991+nlhari@users.noreply.github.com>
2020-01-27 09:39:31 -0500

    Delete api_keys-checkpoint.py

--- WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++
@@ -1,6 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "043132445990660a56d31096b464f971"
-
-# Google API Key
-#g_key = "AIzaSyAEfyuoHLfanOYD8WtJwVCsBYgQyKeKFS8"
-g_key = 'AIzaSyCZwTl-YpHmixZ0p9Y_7So2DKkWzqBeBc4'
